0c136982c46b1048391d6930787ce585fb82f997
md-mohosin <mdmohosin.web@gmail.com>
2025-01-17 02:07:37 +0600

    save add cart data on database and get specific user add item from database

--- index.js
+++ index.js
@@ -70,6 +70,17 @@ async function run() {
 
 
 
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
