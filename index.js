70bdc9e7aa14cf2bb1768a60f010b3fa1902d8d8
md-mohosin <mdmohosin.web@gmail.com>
2025-01-18 01:32:13 +0600

    delete spacific user add item

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 
@@ -78,6 +78,13 @@ async function run() {
         })
 
 
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = cartCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
 
