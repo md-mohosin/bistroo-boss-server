ff46c45a8f8d2133d59761ee7703e312ec4575d5
md-mohosin <mdmohosin.web@gmail.com>
2025-02-01 22:03:28 +0600

    delte spacific item

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 
+const jwt = require('jsonwebtoken');
+
 const app = express()
 
 const port = process.env.PORT || 5000;
@@ -43,13 +45,64 @@ async function run() {
 
 
 
+
+        // MIDDLEWARES
+        const varifyToken = (req, res, next) => {
+            console.log('inside varify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+
+            const token = req.headers.authorization.split(' ')[1]
+
+            jwt.verify(token, process.env.ACCES_TOKEN_SECRET, (error, decoded) => {
+                if (error) {
+                    return res.status(401).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next()
+            })
+        }
+
+
+
+        // USER VARIFY ADMIN AFTER VARIFY TOKEN
+        const varifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            // console.log(email);
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            console.log('user', user);
+            const isAdmin = user.role === 'admin';
+            console.log('isadmin and email', isAdmin, email);
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden acces' })
+            }
+            next()
+        }
+
+
+
+
+
+        // JWT RELETED API
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCES_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+
+
+
+
         // USERS DATA
         app.post('/users', async (req, res) => {
             const user = req.body
             const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query)
             if (existingUser) {
-                return res.send({ message: 'user already taken', insetedId: null })
+                return res.send({ message: 'user already taken', insertedId: null })
             }
             const result = usersCollection.insertOne(user)
             res.send(result)
@@ -57,7 +110,7 @@ async function run() {
 
 
 
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id', varifyToken, varifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
@@ -73,14 +126,31 @@ async function run() {
 
 
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', varifyToken, varifyAdmin, async (req, res) => {
+
             const result = await usersCollection.find().toArray()
             res.send(result)
         })
 
 
+        app.get('/users/admin/:email', varifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'unauthorized access' })
+            }
 
-        app.delete('/users/:id', async (req, res) => {
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            console.log(user, email);
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
+        })
+
+
+        app.delete('/users/:id', varifyToken, varifyAdmin, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await usersCollection.deleteOne(query)
@@ -96,6 +166,21 @@ async function run() {
             res.send(result)
         })
 
+
+        app.post('/menu', varifyToken, varifyAdmin, async (req, res) => {
+            const item = req.body
+            const result = await menuCollection.insertOne(item)
+            res.send(result)
+        })
+
+
+
+        app.delete('/menu/:id', varifyToken, varifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: id }
+            const result = await menuCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
 
